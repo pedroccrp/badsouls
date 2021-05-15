aa9014b719d78774ec5f0ee161185aa838ee7ad3
Pedro Pontes <pontescpedro@gmail.com>
2021-05-15 13:14:39 -0300

    Add routing via url

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+
+import './styles/global.scss';
+
+import Login from 'pages/Login';
+import Register from 'pages/Register';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Bad Souls</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/register' component={Register} />
+        <Route path='/'>
+          <Redirect to='/login' />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
