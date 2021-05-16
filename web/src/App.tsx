18d96bee7a37fd7f2a9b5145890d7443fe285158
Pedro Pontes <pontescpedro@gmail.com>
2021-05-16 16:42:25 -0300

    Add Header to pages and update global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,19 +8,24 @@ import {
 
 import './styles/global.scss';
 
+import Header from 'components/Header';
+
 import Login from 'pages/Login';
 import Register from 'pages/Register';
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Route path='/login' component={Login} />
-        <Route path='/register' component={Register} />
-        <Route path='/'>
-          <Redirect to='/login' />
-        </Route>
-      </Switch>
+      <Header />
+      <div id='app'>
+        <Switch>
+          <Route path='/login' component={Login} />
+          <Route path='/register' component={Register} />
+          <Route path='/'>
+            <Redirect to='/login' />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
