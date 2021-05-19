a84a77c4f839b1b2eb7c42e087e64e2a1f52a2b0
Pedro Pontes <pontescpedro@gmail.com>
2021-05-18 21:42:09 -0300

    Configure test with jest and EC6 and change main file
    Use 'server.ts' as the main file and import app.js to it.

--- server/app.ts
+++ server/app.ts
@@ -9,6 +9,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+export default app;
