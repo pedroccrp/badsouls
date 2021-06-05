093fc307f303713d4ecd9bd277464c5433c31a38
Pedro Pontes <pontescpedro@gmail.com>
2021-06-04 21:46:33 -0300

    Implement using tooltip for InputText component

--- web/src/components/InputText/index.tsx
+++ web/src/components/InputText/index.tsx
@@ -2,23 +2,37 @@ import React from 'react';
 
 import './index.scss';
 
-type InputTextProps = {
-  type?: string;
-  handleChange?: React.ChangeEventHandler<HTMLInputElement>;
-  placeholder?: string;
+type Props = {
+  tooltipLines?: string[];
+} & React.HTMLAttributes<HTMLInputElement>;
+
+const InputText = ({ tooltipLines, ...props }: Props) => {
+  return (
+    <div className='input-field-container'>
+      <input
+        {...props}
+        className={`input-field ${props.className}`}
+        type={props.typeof || 'text'}
+        name={props.typeof || ''}
+        onFocus={e => (e.target.placeholder = '')}
+        onBlur={e => (e.target.placeholder = props.placeholder || '')}
+      />
+
+      {tooltipLines && (
+        <div className='input-field-tooltip'>{displayLines(tooltipLines)}</div>
+      )}
+    </div>
+  );
 };
 
-const InputText = (props: InputTextProps) => {
-  return (
-    <input
-      className='input-field'
-      type={props.type || 'text'}
-      placeholder={props.placeholder || ''}
-      onFocus={e => (e.target.placeholder = '')}
-      onBlur={e => (e.target.placeholder = props.placeholder || '')}
-      onChange={props.handleChange}
-    />
-  );
+const displayLines = (lines: string[]) => {
+  return lines.map((line, index) => {
+    return (
+      <span key={index} className='input-field-tooltip-text'>
+        {line}
+      </span>
+    );
+  });
 };
 
 export default InputText;
